c1440538d661dfc4d35d75c1aa3ce266fc58dba4
Julian <124217830+JulyAn1234@users.noreply.github.com>
2025-01-05 17:12:30 -0800

    Update EcommerceAuthApplication.java

--- src/main/java/com/ecommerceAuth/ecommerceAuth/EcommerceAuthApplication.java
+++ src/main/java/com/ecommerceAuth/ecommerceAuth/EcommerceAuthApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EcommerceAuthApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceAuthApplication.class, args);
 	}
-
 }
